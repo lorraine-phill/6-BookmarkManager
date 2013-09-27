e8ff78cb3170b1496d4d64bf10d4cc970942f80a
lorraine-phill <lp3767-web@yahoo.com>
2013-09-27 11:11:11 +0100

    added user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
 
       include DataMapper::Resource
       	property :id, Serial
-      	property :email, String
+            property :email, String, :unique => true, :message => "This email is already taken"
             property :password_digest, Text
 
       # when assigned the password, we don't store it directly
@@ -27,5 +27,25 @@ class User
         @password = password
         self.password_digest = BCrypt::Password.create(password)
       end
+
+      def self.authenticate(email, password)
+        # that's the user that's trying to sign in
+        user = first(:email => email)
+        # if this user exists and the password provided matches
+        # the one we have password_digest for, everything's fine
+        # The Password.new returns an object that overrides the ==
+        # method. Instead of comparing two passwords directly
+        # (which is impossible because we only have a one-way hash)
+        # the == method calculates the candidate password_digest from
+        # the password given and compares it to the password_digest
+        # it was initialised with.
+        # So, to recap: THIS IS NOT A STRING COMPARISON 
+        if user && BCrypt::Password.new(user.password_digest) == password
+          # return this user
+          user
+        else
+          nil
+        end
+    end
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,5 +1,35 @@
     require 'spec_helper'
 
+      feature "User signs in" do
+
+      before(:each) do
+        User.create(:email => "test@test.com", 
+                    :password => 'test', 
+                    :password_confirmation => 'test')
+      end
+
+      scenario "with correct credentials" do
+        visit '/'
+        expect(page).not_to have_content("Welcome, test@test.com")
+        sign_in('test@test.com', 'test')
+        expect(page).to have_content("Welcome, test@test.com")
+      end
+
+      scenario "with incorrect credentials" do
+        visit '/'
+        expect(page).not_to have_content("Welcome, test@test.com")
+        sign_in('test@test.com', 'wrong')
+        expect(page).not_to have_content("Welcome, test@test.com")
+      end
+
+      def sign_in(email, password)
+        visit '/sessions/new'
+        fill_in 'email', :with => email
+        fill_in 'password', :with => password
+        click_button 'Sign in'
+      end
+
+    end
     
     feature "User signs up" do
 
@@ -26,7 +56,13 @@
       scenario "with a password that doesn't match" do
         lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0) 
         expect(current_path).to eq('/users')   
-        expect(page).to have_content("Sorry, your passwords don't match")
+        expect(page).to have_content("Password does not match the confirmation")
+      end
+
+      scenario "with an email that is already registered" do    
+        lambda { sign_up }.should change(User, :count).by(1)
+        lambda { sign_up }.should change(User, :count).by(0)
+        expect(page).to have_content("This email is already taken")
       end
 
     end
