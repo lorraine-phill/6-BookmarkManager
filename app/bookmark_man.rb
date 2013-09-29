68bd28b9bc5deb59daf8ffeeedb7e12d1704412d
lorraine-phill <lp3767-web@yahoo.com>
2013-09-29 21:02:07 +0100

    no styling

--- app/bookmark_man.rb
+++ app/bookmark_man.rb
@@ -1,5 +1,3 @@
-
-
 require 'sinatra/base'
 require 'sinatra'
 require 'data_mapper'
@@ -15,74 +13,8 @@ require_relative 'data_mapper_setup'
 enable :sessions
 set :session_secret, 'super secret'
 
-get '/' do
-   @links = Link.all
-    erb :index
-end
-
-get '/links/new' do
-  erb :"links/new"
-end
-
-post '/links' do
-  url = params["url"]
-  title = params["title"]
-  tags = params["tags"].split(" ").map{|tag| Tag.first_or_create(:text => tag)}
-  Link.create(:url => url, :title => title, :tags => tags)
-  redirect to('/')
-end
-
-get '/tags/:text' do
-  tag = Tag.first(:text => params[:text])
-  @links = tag ? tag.links : []
-  erb :index
-end
-
-get '/users/new' do
-  @user = User.new
-  erb :"users/new"
-end
-
-post '/users' do
-  # we just initialize the object
-  # without saving it. It may be invalid
-  @user = User.new(:email => params[:email],
-              :password => params[:password],
-              :password_confirmation => params[:password_confirmation])
-  # let's try saving it
-  # if the model is valid,
-  # it will be saved
-  if @user.save
-    session[:user_id] = @user.id
-    redirect to('/')
-    # if it's not valid,
-    # we'll show the same
-    # form again
-  else
-    flash.now[:errors] = @user.errors.full_messages
-    erb :"users/new"
-  end
-end
-
-get '/sessions/new' do
-  erb :"sessions/new"
-end
-
-post '/sessions' do
-  email, password = params[:email], params[:password]
-  user = User.authenticate(email, password)
-
-  if user
-    session[:user_id] = user.id
-    redirect to('/')
-  else
-    flash[:errors] = ["The email or password are incorrect"]
-    erb :"sessions/new"
-  end
-end
-
-delete '/sessions' do
-  flash[:notice] = "Good bye!"
-  session[:user_id] = nil
-  redirect to('/')
-end
+require_relative 'controllers/users'
+require_relative 'controllers/sessions'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/application'
